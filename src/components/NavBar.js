efaaed11226d293db92c3284620730f3c636a2ff
jrso15 <janilleolegario@gmail.com>
2022-07-15 19:01:52 +0800

    navigation with routes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ProductList from "./ProductLists";
+import AddProduct from "./AddProduct";
 import styles from "../styles/NavBar.module.scss";
 
 const NavBar = () => {
@@ -8,24 +11,39 @@ const NavBar = () => {
   };
 
   return (
-    <header>
-      <nav className={styles.nav}>
-        <div className={styles.nav__icon}>
-          <h2 className={styles.nav__icon_title}>e-commerce</h2>
-          <div className={styles.nav__icon_burger} onClick={clickMenu}>
-            <div />
-            <div />
-            <div />
+    <Router>
+      <header>
+        <nav className={styles.nav}>
+          <div className={styles.nav__icon}>
+            <h2 className={styles.nav__icon_title}>e-commerce</h2>
+            <div className={styles.nav__icon_burger} onClick={clickMenu}>
+              <div />
+              <div />
+              <div />
+            </div>
           </div>
-        </div>
 
-        <ul className={styles.nav__list} ref={showMenu}>
-          <li>Home</li>
-          <li>Add Product</li>
-          <li>Cart</li>
-        </ul>
-      </nav>
-    </header>
+          <ul className={styles.nav__list} ref={showMenu}>
+            <li>
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link className="nav-link" to="/addproduct">
+                Add Product
+              </Link>
+            </li>
+            <li>Cart</li>
+          </ul>
+        </nav>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<ProductList />} exact />
+        <Route path="/addproduct" element={<AddProduct />} />
+      </Routes>
+    </Router>
   );
 };
 
